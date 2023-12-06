6104c29166c50263374e84b3d93fb2538537b232
vijay <pothuganti.v@northeastern.edu>
2023-12-06 12:34:03 -0500

    updating

--- application/routes/doctorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DoctorController = require('../controllers/docProfileController');
-
-// POST endpoint for creating a doctor profile
-router.post('/doctors', DoctorController.createDoctor);
-
-
-module.exports = router;
--- application/routes/doctorSearchRoutes.js
+++
@@ -1,9 +0,0 @@
-// doctorRoute.js
-const express = require('express');
-const DoctorController = require('../controllers/docSearchController');
-
-const router = express.Router();
-
-router.get('/search-doctors', DoctorController.searchDoctors);
-
-module.exports = router;
