b2996173febc54863e0750205ea42a7c9e148418
Sandesh Reddy Tummala <tummala.san@northeastern.edu>
2023-12-13 18:14:16 -0500

    UI_i18n_update

--- client/src/home/index.tsx
+++ client/src/home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, ChangeEvent, useRef } from "react";
-import "../Css/Homepage.css"; // Import the corresponding CSS file for styling
+import "../Css/Homepage.css"; // Importing the corresponding CSS file for styling
 import Footer from "../components/Footer";
 import DocBookHeader from "../components/DocBookHeader";
 import { useNavigate } from 'react-router-dom';
@@ -23,12 +23,7 @@ import Psychiatrist from "../media/Psychiatrist.jpg";
 import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { faCircleArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-type ExpandedState = {
-  medical: boolean;
-  dental: boolean;
-  mentalHealth: boolean;
-  vision: boolean;
-};
+
 
 interface User {
   location: {
@@ -63,6 +58,7 @@ interface User {
   __v: number;
 }
 
+//Initialising the homepage
 const HomePage = () => {
   const [searchParams, setSearchParams] = useState({
     name: "",
@@ -70,13 +66,16 @@ const HomePage = () => {
     location: "",
   });
 
-  //const { t } = useTranslation('common');
+  //Internationalisation variable
+  const { t, i18n, ready } = useTranslation("common", { useSuspense: false });
 
+  //Variables declared
   const [users, setUsers] = useState<User[]>([]);
-  const minRating = 0; 
+  const minRating = 4.5; 
   const targetSpecialty = "Cardio";
   const targetSpecialty2 = "Dent";
 
+  //Seach Bar function
   const navigateToDoctorSearch = (searchCriteria: SearchCriteria) => {
     navigate("/search", { state: { searchCriteria } });
   };
@@ -103,6 +102,7 @@ const HomePage = () => {
 
   const filteredUsersByRating = filterUsersByRating();
 
+  //Function to filter based on specialty
   const filterUsersBySpecialty = (tar: string) => {
     //return users.filter((user) => user.specialty === tar);
 
@@ -112,72 +112,33 @@ const HomePage = () => {
     });
   };
 
-
+  //Functions for Cardiologists, Dentists 
   const filteredUsersBySpecialty = filterUsersBySpecialty(targetSpecialty);
   const filteredUsersBySpecialty2 = filterUsersBySpecialty(targetSpecialty2);
 
-  const allSpecialties = [
-    "Primary Care",
-    "Dentist",
-    "Dermatologist",
-    "Psychiatrist",
-    "Opthalmologist",
-  ];
+  //Navigation to other pages
   const navigate = useNavigate();
 
   const handleCardClick = (doctorId: number) => {
     navigate(`/doctorprofile/doctor/${doctorId}`);
   };
 
-  const [expanded, setExpanded] = useState<ExpandedState>({
-    medical: false,
-    dental: false,
-    mentalHealth: false,
-    vision: false,
-  });
-
   const handleRedirect = (path: string) => {
     navigate(path);
-  };
-
-  const toggleExpand = (reason: keyof ExpandedState) => {
-    setExpanded((prevExpanded) => ({
-      ...prevExpanded,
-      [reason]: !prevExpanded[reason],
-    }));
   };
 
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setSearchParams({ ...searchParams, [name]: value });
   };
-
-  const topRatedDoctors = [
-    { id: 1, name: "Dr. John Doe", specialty: "Cardiologist" },
-    { id: 2, name: "Dr. Jane Smith", specialty: "Dermatologist" },
-    { id: 3, name: "Dr. John Doe", specialty: "Cardiologist" },
-    { id: 4, name: "Dr. Jane Smith", specialty: "Dermatologist" },
-    { id: 5, name: "Dr. John Doe", specialty: "Cardiologist" },
-    { id: 6, name: "Dr. Jane Smith", specialty: "Dermatologist" },
-    { id: 7, name: "Dr. John Doe", specialty: "Cardiologist" },
-    { id: 8, name: "Dr. Jane Smith", specialty: "Dermatologist" },
-    { id: 9, name: "Dr. John Doe", specialty: "Cardiologist" },
-    { id: 10, name: "Dr. Jane Smith", specialty: "Dermatologist" },
-    { id: 11, name: "Dr. John Doe", specialty: "Cardiologist" },
-    { id: 12, name: "Dr. Jane Smith", specialty: "Dermatologist" },
-    // Add more top-rated doctor profiles here
-  ];
-
-  const dentists = [
-    { id: 3, name: "Dr. Alex Johnson", specialty: "Dentist" },
-    { id: 4, name: "Dr. Emily Brown", specialty: "Orthodontist" },
-    // Add more dentist profiles here
-  ];
+  
 
   const doctorScrollRef = useRef<HTMLDivElement | null>(null);
   const dentistScrollRef = useRef<HTMLDivElement | null>(null);
   const cardioScrollRef = useRef<HTMLDivElement | null>(null);
 
+
+  //Scroll functionalities
   const scrollLeft = (ref: React.RefObject<HTMLDivElement> | null) => {
     if (ref && ref.current) {
       ref.current.scrollTo({
@@ -196,68 +157,12 @@ const HomePage = () => {
     }
   };
 
-  const topSearchedSpecialties = [
-    {
-      id: 1,
-      name: "Orthodontics",
-      logo: "path/to/orthodontics-logo.png",
-      pageUrl: "/orthodontics-page",
-    },
-    {
-      id: 2,
-      name: "Pediatric Dentistry",
-      logo: "path/to/pediatric-dentistry-logo.png",
-      pageUrl: "/orthodontics-page",
-    },
-    {
-      id: 1,
-      name: "Orthodontics",
-      logo: "path/to/orthodontics-logo.png",
-      pageUrl: "/orthodontics-page",
-    },
-    {
-      id: 2,
-      name: "Pediatric Dentistry",
-      logo: "path/to/pediatric-dentistry-logo.png",
-      pageUrl: "/orthodontics-page",
-    },
-
-    // Add more specialties
-  ];
-
-  const doctorCards = [
-    {
-      id: 1,
-      doctorName: "Doctor 1",
-      doctorLogo: "path/to/doctor1-logo.png",
-      doctorDescription: "Description for Doctor 1",
-      buttonLabel: "View Profile",
-      // Add more properties as needed for other cards
-    },
-    {
-      id: 2,
-      doctorName: "Doctor 2",
-      doctorLogo: "path/to/doctor2-logo.png",
-      doctorDescription: "Description for Doctor 2",
-      buttonLabel: "View Profile",
-      // Add more properties as needed for other cards
-    },
-    {
-      id: 3,
-      doctorName: "Doctor 3",
-      doctorLogo: "path/to/doctor3-logo.png",
-      doctorDescription: "Description for Doctor 3",
-      buttonLabel: "View Profile",
-      // Add more properties as needed for other cards
-    },
-    // Add more doctor cards
-  ];
 
   return (
     <div className="homepage">
         <div className="upper">
             <DocBookHeader></DocBookHeader>
-            <div className='uppertext'>Book local doctors near you</div>
+            <div className='uppertext'>{t("home.page.title")}</div>
             <div className='searchframe'>
                 <div className="search-bar">
                     <div className="search-icon">
@@ -305,8 +210,8 @@ const HomePage = () => {
         <div className="doctor-profiles">
             <div className="docrow-display">
                 <div className="doctor-row-label">
-                    Top Rated Doctors
-                <h3> 90% patients rated 5-stars</h3>
+                    {t("home.page.toprated")}
+                <h3> {t("home.page.toprdesc")}</h3>
                 </div>
                 <div className="doctor-row">
                     <div className="doctor-cards" ref={doctorScrollRef}>
@@ -329,9 +234,6 @@ const HomePage = () => {
                                 </div>
                               ))}</div>
                             </div>
-                            {/* <div className="carddoc-footer">
-                              Check Availablity
-                            </div> */}
                           </div>
                         ))}   
                     </div>
@@ -352,7 +254,7 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="docrow-display">
-                <div className="doctor-row-label">Dentists</div>
+                <div className="doctor-row-label">{t("home.page.dentist")}</div>
                 <div className="doctor-row">
                     <div className="doctor-cards" ref={dentistScrollRef}>
                         {filteredUsersBySpecialty2.map(user => (
@@ -366,16 +268,13 @@ const HomePage = () => {
                             </div>
                             <div className="card-body">
                               <h2 className="card-title"><FontAwesomeIcon icon={faStar} /> {user.rating} Rating</h2>
-                              <h2 className="card-title"><FontAwesomeIcon icon={faEnvelope} /> {user.email}</h2>
+                              <p className="card-title2"><FontAwesomeIcon icon={faEnvelope} /> {user.email}</p>
                               <div className="card-text">{user.experience.map(value => (
                                 <div className="exp">
                                   <p>{value.duration} In {value.hospital}</p>
                                 </div>
                               ))}</div>
                             </div>
-                            {/* <div className="carddoc-footer">
-                              Check Availablity
-                            </div> */}
                           </div>
                         ))}   
                         
@@ -397,7 +296,7 @@ const HomePage = () => {
                 </div>
             </div>
             <div className="docrow-display">
-                <div className="doctor-row-label">Cardiologists</div>
+                <div className="doctor-row-label">{t("home.page.cardiologists")}</div>
                 <div className="doctor-row">
                     <div className="doctor-cards" ref={cardioScrollRef}>
                         {filteredUsersBySpecialty.map(user => (
@@ -412,7 +311,7 @@ const HomePage = () => {
                             </div>
                             <div className="card-body">
                               <h2 className="card-title"><FontAwesomeIcon icon={faStar} /> {user.rating} Rating</h2>
-                              <h2 className="card-title"><FontAwesomeIcon icon={faEnvelope} /> {user.email}</h2>
+                              <h2 className="card-title2"><FontAwesomeIcon icon={faEnvelope} /> {user.email}</h2>
                               <div className="card-text">{user.experience.map(value => (
                                 <div className="exp">
                                   <p>{value.duration} In {value.hospital}</p>
@@ -444,7 +343,7 @@ const HomePage = () => {
             </div>
         </div>
         <div className="doctor-spec">
-            <div className="doctor-row-label">Top Searched Specialties</div>
+            <div className="doctor-row-label">{t("home.page.spec.title")}</div>
             <div className="specialty-cards">
                 <a href="/search" onClick={(e) => {
                     e.preventDefault();
@@ -452,7 +351,7 @@ const HomePage = () => {
                 }} className="specialty-card-link">
                 <div className="specialty-card">
                     <img src={PrimaryCare}></img>
-                    <div className="specialty-name">Primary Care</div>
+                    <div className="specialty-name">{t("home.page.speccard.pc")}</div>
                 </div>
                 </a>
 
@@ -470,7 +369,7 @@ const HomePage = () => {
           >
             <div className="specialty-card">
               <img src={Dentist}></img>
-              <div className="specialty-name">Dentist</div>
+              <div className="specialty-name">{t("home.page.speccard.den")}</div>
             </div>
           </a>
 
@@ -488,7 +387,7 @@ const HomePage = () => {
           >
             <div className="specialty-card">
               <img src={OBGYN}></img>
-              <div className="specialty-name">OB-GYN</div>
+              <div className="specialty-name">{t("home.page.speccard.ob")}</div>
             </div>
           </a>
 
@@ -506,7 +405,7 @@ const HomePage = () => {
           >
             <div className="specialty-card">
               <img src={Psychiatrist}></img>
-              <div className="specialty-name">Psychiatrist</div>
+              <div className="specialty-name">{t("home.page.speccard.psy")}</div>
             </div>
           </a>
 
@@ -524,37 +423,17 @@ const HomePage = () => {
           >
             <div className="specialty-card">
               <img src={Cardiologist}></img>
-              <div className="specialty-name">Cardiologist</div>
+              <div className="specialty-name">{t("home.page.speccard.car")}</div>
             </div>
           </a>
         </div>
       </div>
-      {/* <div className="doctor-section">
-            <h2 className="doctor-section-title">Doctors</h2>
-            <div className="doctor-cards-container">
-            {doctorCards.map((doctor) => (
-                <div className="doctor-card" key={doctor.id}>
-                <img
-                    src={doctor.doctorLogo}
-                    alt={doctor.doctorName}
-                    className="doctor-logo"
-                />
-                <div className="doctor-info">
-                    <h3 className="doctor-name">{doctor.doctorName}</h3>
-                    <p className="doctor-description">{doctor.doctorDescription}</p>
-                    <button className="doctor-btn">{doctor.buttonLabel}</button>
-                </div>
-                </div>
-            ))}
-            </div>
-        </div> */}
       <div className="information-section">
         <div className="left-content">
-          <h2 className="left-title">Thousands of providers.</h2>
-          <h2>One app.</h2>
+          <h2 className="left-title">{t("home.page.info.title1")}</h2>
+          <h2>{t("home.page.info.title2")}</h2>
           <p className="left-text">
-            The DocBook app is the quickest, easiest way to book and keep track
-            of your appointments.
+            {t("home.page.info.desc")}
           </p>
           <img src={qrimg} alt="Content Image" className="left-image" />
           <div className="buttons-container">
